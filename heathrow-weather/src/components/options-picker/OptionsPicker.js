40338d4778756d85273e1ad48e3c18628bc684cd
Davide Zordan <mail@davide.dev>
2020-01-13 18:55:06 +0000

    Refactoring using React Hooks

--- heathrow-weather/src/components/options-picker/OptionsPicker.js
+++ heathrow-weather/src/components/options-picker/OptionsPicker.js
@@ -1,82 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Header, Segment, Button, Form, Radio } from "semantic-ui-react";
 import DatePickerRange from '../date-picker-range/DatePickerRange';
 
-class OptionPicker extends React.PureComponent {
+const OptionPicker = ({ onSubmit }) => {
+    const [ serieValue, setSerieValue ] = useState([]);
+    const [ dateValue, setDateValue ] = useState('');
 
-    constructor(props) {
-        super(props);
+    const handleSerieChange = (e, { value }) => setSerieValue(value);
 
-        this.state = { serieValue: '', dateValue: [] };
+    const handleDateChange = (e, { value }) => setDateValue(value);
 
-        this.handleSerieChange = (e, { value }) => this.setState({ serieValue: value });
+    const handleClick = (e, { value }) => {
+        onSubmit({ serie: serieValue, date: dateValue });
+    };
 
-        this.handleDateChange = (e, { value }) => this.setState({ dateValue: value });
+    return (
+        <Segment>
+            <Form>
+                <Form.Field required>
+                    <Header as="h4">Date range:</Header>
+                    <DatePickerRange fluid onChange={handleDateChange} />
+                </Form.Field>
 
-        this.handleClick = (e, { value }) => {
-            const { serieValue, dateValue } = this.state;
-            props.onSubmit({ serie: serieValue, date: dateValue });
-        };
-    }
+                <Form.Field>
+                    <Radio
+                        label='Temp max (°C)'
+                        name='radioGroup'
+                        value='tmax_C'
+                        checked={serieValue === 'tmax_C'}
+                        onChange={handleSerieChange}
+                    />
+                </Form.Field>
 
-    render() {
-        const { serieValue } = this.state;
+                <Form.Field>
+                    <Radio
+                        label='Temp min (°C)'
+                        name='radioGroup'
+                        value='tmin_C'
+                        checked={serieValue === 'tmin_C'}
+                        onChange={handleSerieChange}
+                    />
+                </Form.Field>
 
-        return (
-            <Segment>
-                <Form>
-                    <Form.Field required>
-                        <Header as="h4">Date range:</Header>
-                        <DatePickerRange fluid onChange={this.handleDateChange} />
-                    </Form.Field>
+                <Form.Field>
+                    <Radio
+                        label='Rain max (mm)'
+                        name='radioGroup'
+                        value='rain_mm'
+                        checked={serieValue === 'rain_mm'}
+                        onChange={handleSerieChange}
+                    />
+                </Form.Field>
 
-                    <Form.Field>
-                        <Radio
-                            label='Temp max (°C)'
-                            name='radioGroup'
-                            value='tmax_C'
-                            checked={serieValue === 'tmax_C'}
-                            onChange={this.handleSerieChange}
-                        />
-                    </Form.Field>
+                <Form.Field>
+                    <Radio
+                        label='Sunshine hours'
+                        name='radioGroup'
+                        value='sunshine_hours'
+                        checked={serieValue === 'sunshine_hours'}
+                        onChange={handleSerieChange}
+                    />
+                </Form.Field>
+                
+                <Button fluid onClick={handleClick}>Display</Button>
 
-                    <Form.Field>
-                        <Radio
-                            label='Temp min (°C)'
-                            name='radioGroup'
-                            value='tmin_C'
-                            checked={serieValue === 'tmin_C'}
-                            onChange={this.handleSerieChange}
-                        />
-                    </Form.Field>
-
-                    <Form.Field>
-                        <Radio
-                            label='Rain max (mm)'
-                            name='radioGroup'
-                            value='rain_mm'
-                            checked={serieValue === 'rain_mm'}
-                            onChange={this.handleSerieChange}
-                        />
-                    </Form.Field>
-
-                    <Form.Field>
-                        <Radio
-                            label='Sunshine hours'
-                            name='radioGroup'
-                            value='sunshine_hours'
-                            checked={serieValue === 'sunshine_hours'}
-                            onChange={this.handleSerieChange}
-                        />
-                    </Form.Field>
-                    
-                    <Button fluid onClick={this.handleClick}>Display</Button>
-
-                </Form>
-            </Segment>
-        );
-    }
+            </Form>
+        </Segment>
+    );
 }
 
 export default OptionPicker;
--- heathrow-weather/src/components/weather-chart/WeatherChart.js
+++ heathrow-weather/src/components/weather-chart/WeatherChart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import getData from '../../data/DataService.js'
 
 import { Segment } from 'semantic-ui-react';
@@ -12,66 +12,58 @@ import {
 } from 'react-vis';
 import '../../../node_modules/react-vis/dist/style.css';
 
-class WeatherChart extends React.Component {
-  state = {
-    useCanvas: false,
-    height: 0,
-    width: 0
-  };  
+const WeatherChart = ({ requiredSeries }) => {
+  const [ height, setHeight ] = useState(0);
 
-  componentDidMount = () => {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-  }
-
-  componentWillUnmount = () => {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions = () => {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  const updateWindowDimensions = () => {
+    setHeight(window.innerHeight);
   };
 
-  render = () => {
-    const { requiredSeries } = this.props;
-    const { height } = this.state;
-
-    if (!requiredSeries.serie || (requiredSeries.date.length !== 2)) {
-      return <Segment>Please select chart options</Segment>;
+  useEffect(() => {
+      updateWindowDimensions();
+      window.addEventListener('resize', updateWindowDimensions);
+    return () => {
+      window.removeEventListener('resize', updateWindowDimensions);
     }
+  }, []);
 
-    const graphData = getData(requiredSeries.date[0], requiredSeries.date[1], requiredSeries.serie);
-    if (!graphData || graphData.length === 0) {
-      return <Segment>No data available</Segment>;
-    }
+  const { serie, date } = requiredSeries;
 
-    return (
-      <Segment>
-        <FlexibleXYPlot
-          xType="time"
-          height={height * 0.6}
-          yType="linear"
-          xDistance={150}>
-
-          <HorizontalGridLines />
-          <VerticalGridLines />
-          <XAxis />
-          <YAxis />
-
-          <LineMarkSeries
-            className="linemark-series-example"
-            style={{
-              strokeWidth: '3px'
-            }}
-            lineStyle={{stroke: 'red'}}
-            markStyle={{stroke: 'blue'}}
-            data={graphData}
-            animation
-            />
-        </FlexibleXYPlot>
-      </Segment>
-    );
+  if (!serie || (date.length !== 2)) {
+    return <Segment>Please select chart options</Segment>;
   }
+
+  const graphData = getData(date[0], date[1], serie);
+  if (!graphData || graphData.length === 0) {
+    return <Segment>No data available</Segment>;
+  }
+
+  return (
+    <Segment>
+      <FlexibleXYPlot
+        xType="time"
+        height={height * 0.6}
+        yType="linear"
+        xDistance={150}>
+
+        <HorizontalGridLines />
+        <VerticalGridLines />
+        <XAxis />
+        <YAxis />
+
+        <LineMarkSeries
+          className="linemark-series-example"
+          style={{
+            strokeWidth: '3px'
+          }}
+          lineStyle={{stroke: 'red'}}
+          markStyle={{stroke: 'blue'}}
+          data={graphData}
+          animation
+          />
+      </FlexibleXYPlot>
+    </Segment>
+  );
 }
 
 export default WeatherChart;
